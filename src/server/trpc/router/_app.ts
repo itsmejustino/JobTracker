9a61142e85dedfd8910d36d934d3069288cb73e5
itsmejustino <justindortiz@me.com>
2022-11-22 14:54:34 -0700

    create job post mutation

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,8 +1,10 @@
 import { router } from "../trpc";
 import { exampleRouter } from "./example";
+import { jobRouter } from "./jobrouter";
 
 export const appRouter = router({
   example: exampleRouter,
+  jobs: jobRouter,
 });
 
 // export type definition of API
--- src/server/trpc/router/jobrouter.ts
+++ src/server/trpc/router/jobrouter.ts
@@ -1,21 +1,30 @@
 import { z } from "zod";
-import { createRouter } from "../context";
+import { router, publicProcedure } from "../trpc";
 
-const jobRouter = createRouter().mutation('addJob', {
-    input: z.object({
-        name: z.string(),
-    }),
-    resolve: async ({ input, ctx }) => {
-        const { name } = input
-        await ctx.prisma.item.create({
-            data: {
-                name,
-                company,
-                platform,
-                appliedon,
 
-            },
+export const jobRouter = router({
+    // Create procedure at path 'login'
+    // The syntax is identical to creating queries
+    login: publicProcedure
+        // using zod schema to validate and infer input values
+        .input(
+            z.object({
+                name: z.string(),
+                company: z.string(),
+                platform: z.string(),
+                appliedon: z.string(),
+            })
+        )
+        .mutation(({ input }) => {
+            // Here some login stuff would happen
+            return {
+                user: {
+                    name: input.name,
+                    company: input.company,
+                    platform: input.platform,
+                    aplliedon: input.appliedon,
+                },
+            };
         })
-    },
+});
 
-})
