48bc6d33713a98482cb885aff2a7002c05b1d2c6
itsmejustino <justindortiz@me.com>
2022-11-23 14:51:27 -0700

    list styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,34 +16,46 @@ const Home: NextPage = () => {
       </Head>
       <main className="mx-auto my-12 nav-w-3xl">
         <div className="flex justify-around p-3 gap-3">
-          <h2 className="text-2xl font-semibold">My applications</h2>
-          <button type='button' className="bg-blue-400 text-sm rounded-md transition p-2 hover:bg-blue-500">Add new job</button>
-
-
+          <h2 className="text-2xl font-semibold">Applications Tracker</h2>
+          <div className="flex flex-row">
+            <button type='button' className="flex flex-row items-center gap-2 bg-blue-400 text-sm rounded-md transition p-2 hover:bg-blue-500">Save Applications List<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+            </svg>
+          
+        </button>
         </div>
-        <div className="flex flex-wrap flex-row gap-4 mt-20 justify-center">
-          <div className="flex flex-col gap-2">
+
+      </div>
+      <div className="flex flex-wrap flex-row gap-4 mt-20 justify-center items-end ">
+        <div className="flex flex-col gap-2">
           <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Job Name</label>
           <input type="text" id="small-input" className="block w-100 p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-          </div>
-       
-          <div className="flex flex-col gap-2">
-          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Job Name</label>
-          <input type="text" id="small-input" className="block w-100 p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-          </div>
-        
-          <div className="flex flex-col gap-2">
-          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Job Name</label>
-          <input type="text" id="small-input" className="block w-100 p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-          </div>
-        
-        
-          <div className="flex flex-col gap-2">
-          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Job Name</label>
-          <input type="text" id="small-input" className="block w-100 p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-          </div>
         </div>
-      </main>
+
+        <div className="flex flex-col gap-2">
+          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Organization</label>
+          <input type="text" id="small-input" className="block w-100 p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Platform</label>
+          <input type="text" id="small-input" className="block w-100 p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Applied On-Date</label>
+          <input type="text" id="small-input" className="block w-100 p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+        </div>
+        <div className="">
+        <button type='button' className="flex flex-row items-center gap-2 bg-blue-400 text-sm rounded-md transition p-2 hover:bg-blue-500">Add Job <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+            </svg>
+          
+        </button>
+        </div>
+
+      </div>
+    </main>
     </>
   );
 };
